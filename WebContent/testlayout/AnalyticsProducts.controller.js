6cbbf75b6a1456265029b1ad9387135369b104db
Johannes Neumann <joeger518@yahoo.de>
2014-11-13 21:45:14 +0100

    Changelog:
    AnalyticsMaster:
    Changed Selection Mode of list to SingleSelectMaster
    Replaced press event with select event due to singleSelectMaster
    Changed Model names to German
    Added Footer: Now is the new "Zurück" button
    
    
    AnalyticsProducts
    
    Added table tab (model used)
    Create timeline item (hard coded in xml, no model used)
    created pie chart (possible to switch between visit / unique visits /
    rating, model used in AnalyticsProducts.controller)
    Crea
    
    
    Misc:
    
    Add fragment folder
    Included HelpDialog.fragment.xml (used for explanation in "Wer sucht
    was" panel)
    
    
    AnalyticsGeneral:
    
    Removed "Zürück" button (added in Masterview)

--- WebContent/testlayout/AnalyticsProducts.controller.js
+++ WebContent/testlayout/AnalyticsProducts.controller.js
@@ -1,38 +1,193 @@
 sap.ui.controller("testApp.testlayout.AnalyticsProducts", {
 
-/**
-* Called when a controller is instantiated and its View controls (if available) are already created.
-* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf testlayout.AnalyticsProducts
-*/
-//	onInit: function() {
-//
-//	},
+	/**
+	 * Called when a controller is instantiated and its View controls (if
+	 * available) are already created. Can be used to modify the View before it
+	 * is displayed, to bind event handlers and do other one-time
+	 * initialization.
+	 * 
+	 * @memberOf piechartmulti.main
+	 */
+	onInit : function() {
 
-/**
-* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-* (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf testlayout.AnalyticsProducts
-*/
-//	onBeforeRendering: function() {
-//
-//	},
+		var jsonString = {
+			Fruits : [ {
+				ProductId : "0",
+				Product : "Kirschen",
+				Visits : "10",
+				UniqueVisits : "88",
+				AverageRating : "3.52"
+			}, {
+				ProductId : "1",
+				Product : "Äpfel",
+				Visits : "240",
+				UniqueVisits : "100",
+				AverageRating : "4.52"
+			}, {
+				ProductId : "2",
+				Product : "Bananen",
+				Visits : "500",
+				UniqueVisits : "200",
+				AverageRating : "4.11"
+			} ],
+			DataSets : [ {
+				dataSetId : "d1",
+				name : "Seitenaufrufe"
 
-/**
-* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-* This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf testlayout.AnalyticsProducts
-*/
-//	onAfterRendering: function() {
-//
-//	},
+			}, {
+				dataSetId : "d2",
+				name : "Indiv. Seitenaufrufe"
 
-/**
-* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf testlayout.AnalyticsProducts
-*/
-//	onExit: function() {
-//
-//	}
+			}, {
+				dataSetId : "d3",
+				name : "Durchschnittsbewertung"
+
+			} ],
+			Table : [ {
+				ProductGroup : "Apfel",
+				Product : "Pink Lady, 6",
+				Visitors : "2032",
+				UniqueVisitors : "960",
+				Price: "1.20",
+				Currency: "EUR"
+			}, {
+				ProductGroup : "Apfel",
+				Product : "Arlet, 6",
+				Visitors : "1232",
+				UniqueVisitors : "920",
+					Price: "1.85",
+					Currency: "EUR"
+			}, {
+				ProductGroup : "Apfel",
+				Product : "Brettacher, 6",
+				Visitors : "500",
+				UniqueVisitors : "124",
+				Price: "2.30",
+				Currency: "EUR"
+			}, {
+				ProductGroup : "Apfel",
+				Product : "Boskop, 6",
+				Visitors : "990",
+				UniqueVisitors : "321",
+				Price: "3.20",
+				Currency: "EUR"
+			},
+			{
+				ProductGroup : "Milch",
+				Product : "Fettarme Milch, 1 Liter",
+				Visitors : "990",
+				UniqueVisitors : "321",
+				Price: "1.22",
+				Currency: "EUR"
+			} ,
+			{
+				ProductGroup : "Milch",
+				Product : "Vollmilch, 1 Liter",
+				Visitors : "990",
+				UniqueVisitors : "321",
+				Price: "1.45",
+				Currency: "EUR"
+			},
+			{
+				ProductGroup : "Milch",
+				Product : "Vollmilch 1.5% Fett, 1 Liter",
+				Visitors : "1090",
+				UniqueVisitors : "621",
+				Price: "2.45",
+				Currency: "EUR"
+			}]
+
+		};
+		this.oModel1 = new sap.ui.model.json.JSONModel();
+		this.oModel1.setData(jsonString);
+		this.getView().setModel(this.oModel1); // set Model to this view only
+
+		// data set
+		var dataset1 = new sap.viz.ui5.data.FlattenedDataset("d1", {
+
+			dimensions : [ {
+				axis : 1,
+				name : 'Obst',
+				value : "{Product}"
+			} ],
+
+			measures : [ {
+				name : 'Seitenaufrufe',
+				value : '{Visits}'
+			} ],
+
+			data : {
+				path : "/Fruits"
+			}
+		});
+		var dataset2 = new sap.viz.ui5.data.FlattenedDataset("d2", {
+
+			dimensions : [ {
+				axis : 1,
+				name : 'Obst',
+				value : "{Product}"
+			} ],
+
+			measures : [ {
+				name : 'Individuelle Seitenaufrufe',
+				value : '{UniqueVisits}'
+			} ],
+
+			data : {
+				path : "/Fruits"
+			}
+
+		});
+		var dataset3 = new sap.viz.ui5.data.FlattenedDataset("d3", {
+
+			dimensions : [ {
+				axis : 1,
+				name : 'Obst',
+				value : "{Product}"
+			} ],
+
+			measures : [ {
+				name : 'Durchschnittsbewertung',
+				value : '{AverageRating}'
+			} ],
+
+			data : {
+				path : "/Fruits"
+			}
+
+		});
+		console.log(this.getView().byId("xmlPie"))
+		this.getView().byId("xmlPie").setTitle(new sap.viz.ui5.types.Title({
+			text : "Obst im Überblick",
+			visible : true
+		}))
+		this.getView().byId("xmlPie").setDataset(dataset1)
+	},
+
+	onChange : function() {
+
+		var key = this.getView().byId("select").getSelectedItem().getKey()
+		var selectedDataSet = sap.ui.getCore().byId(key);
+		this.getView().byId("xmlPie").setDataset(selectedDataSet)
+
+	},
+
+	openDialog : function() {
+
+		if (!this.helpDialog) {
+			this.helpDialog = sap.ui.xmlfragment(
+					"testApp.testlayout.fragments.HelpDialog", this);
+		}
+		this.helpDialog.open()
+	},
+
+	onMsgDialogPress : function(oEvent) {
+		console.log("hi")
+		this.openDialog();
+	},
+
+	onDialogCloseButton : function(oEvent) {
+		this.helpDialog.close();
+	}
 
 });
