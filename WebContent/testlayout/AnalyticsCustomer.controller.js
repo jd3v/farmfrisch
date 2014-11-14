1ecd0a71347ebb3c5c651aba019eba20c058338a
Teena <teenaphil@gmail.com>
2014-11-14 10:11:43 +0100

    Modified AnalyticsCustomer to Show Customer Reviews

--- WebContent/testlayout/AnalyticsCustomer.controller.js
+++ WebContent/testlayout/AnalyticsCustomer.controller.js
@@ -8,7 +8,140 @@ sap.ui.controller("testApp.testlayout.AnalyticsCustomer", {
 //	onInit: function() {
 //
 //	},
+	onPress: function (evt) {
 
+		
+		   jQuery.sap.require("sap.m.MessageToast");
+		   var button = evt.getSource().getId() ;
+		   var element;
+
+		   var reviews5 =[{
+				"ReviewId" : "0",
+				"Sender" : "Giselle Ashante-Ramirez",
+				"Icon" : "img/Customers/Sender1.jpg",
+				"Info" : "Request",
+				"Timestamp" : "November 11, 2014",
+				"Text" :"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. At vero eos et accusam et justo duo dolores et ea rebum."	
+			} ,
+			
+			{
+				"ReviewId" : "1",
+				"Sender" : "johannes Schaffensteiger",
+				"Icon" : "img/Customers/Sender2.jpg",
+				"Info" : "Reply",
+				"Timestamp" : "October 10, 2014",
+				"Text" :"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. At vero eos et accusam et justo duo dolores et ea rebum."	
+			}
+
+		];
+		  
+		 		
+
+		var reviews4 =               [{
+			"ReviewId" : "0",
+			"Sender" : "Sender A",
+			"Icon" : "img/Customers/Sender A.jpg",
+			"Info" : "Request",
+			"Timestamp" : "August 15, 2014",
+			"Text" :"Review for 4 Stars"	
+		} ,
+
+		{
+			"ReviewId" : "1",
+			"Sender" : "Sender B",
+			"Icon" : "img/Customers/Sender B.jpg",
+			"Info" : "Reply",
+			"Timestamp" : "September 08, 2014",
+			"Text" :"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. At vero eos et accusam et justo duo dolores et ea rebum."	
+		}
+
+		];	
+
+		var reviews3 = [{
+			"ReviewId" : "0",
+			"Sender" : "Sender C",
+			"Icon" : "img/Customers/Sender C.jpg",
+			"Info" : "Request",
+			"Timestamp" : "March 03, 2013",
+			"Text" :"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. At vero eos et accusam et justo duo dolores et ea rebum."	
+		} ,
+
+		{
+			"ReviewId" : "1",
+			"Sender" : "Sender D",
+			"Icon" : "img/Customers/Sender D.jpg",
+			"Info" : "Reply",
+			"Timestamp" : "March 04, 2013",
+			"Text" :"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. At vero eos et accusam et justo duo dolores et ea rebum."	
+		}
+
+		];	
+
+		var reviews2        =        [{
+			"ReviewId" : "0",
+			"Sender" : "Sender E",
+			"Icon" : "img/Customers/Sender1.jpg",
+			"Info" : "Request",
+			"Timestamp" : "March 03, 2013",
+			"Text" :"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. At vero eos et accusam et justo duo dolores et ea rebum."	
+		} ,
+
+		{
+			"ReviewId" : "1",
+			"Sender" : "Sender F",
+			"Icon" : "img/Customers/Sender2.jpg",
+			"Info" : "Reply",
+			"Timestamp" : "March 04, 2013",
+			"Text" :"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. At vero eos et accusam et justo duo dolores et ea rebum."	
+		}];
+		
+		 var reviews1 = [{
+				"ReviewId" : "0",
+				"Sender" : "Sender G",
+				"Icon" : "img/Customers/Sender1.jpg",
+				"Info" : "Request",
+				"Timestamp" : "March 03, 2013",
+				"Text" :"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. At vero eos et accusam et justo duo dolores et ea rebum."	
+			} ,
+
+			{
+				"ReviewId" : "1",
+				"Sender" : "Sender H",
+				"Icon" : "img/Customers/Sender2.jpg",
+				"Info" : "Reply",
+				"Timestamp" : "March 04, 2013",
+				"Text" :"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. At vero eos et accusam et justo duo dolores et ea rebum."	
+			}];
+		  
+
+		switch (button)
+		   {
+		     case "__button0": reviews = reviews5 ;break;
+		     case "__button1": reviews = reviews4 ;break;
+		     case "__button2": reviews = reviews3 ;break;
+		     case "__button3": reviews = reviews2 ;break;
+		     case "__button4": reviews = reviews1 ;break;
+		
+		   }	
+		   
+		   var model = new sap.ui.model.json.JSONModel(reviews);
+		   
+		   
+			   if (! this._oPopover) {
+				      this._oPopover = sap.ui.xmlfragment("testApp.testlayout.fragments.Popover", this);
+				      this.getView().addDependent(this._oPopover);
+				     
+				      
+			      
+			   }
+			   
+			   this._oPopover.setModel(model);
+			   var oButton = evt.getSource();
+			   this._oPopover.openBy(oButton);
+			   
+
+			    
+		},
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 * (NOT before the first rendering! onInit() is used for that one!).
